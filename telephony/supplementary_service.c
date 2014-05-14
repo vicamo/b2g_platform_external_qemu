e874f1da8d31badf3f17edf9879add102651f95b
Edgar Chen <echen@mozilla.com>
2014-05-14 14:36:00 +0800

    Bug 861725 - Part 2: Support call forwarding. r=vicamo

--- telephony/supplementary_service.c
+++ telephony/supplementary_service.c
@@ -20,18 +20,106 @@
 #endif
 
 typedef struct ASupplementaryServiceRec_ {
+    /* call forward conditions */
+    ACallForwardRec*  call_forward[CALL_FORWARDING_MAX_REASON + 1]
+                                  [CALL_FORWARDING_MAX_CLASSX_OFFSET + 1];
 } ASupplementaryServiceRec;
 
 static ASupplementaryServiceRec  _s_supplementary[MAX_GSM_DEVICES];
 
+static int
+asupplementary_callforward_init(ASupplementaryService supplementary)
+{
+    // Init call forward record.
+    memset(supplementary->call_forward, 0, sizeof(supplementary->call_forward));
+}
+
+static void
+asupplementary_callforward_destroy(ASupplementaryService supplementary)
+{
+    int i = 0;
+    int j = 0;
+
+    for (i = 0; i <= CALL_FORWARDING_MAX_REASON; i++) {
+        for (j = 0; j <= CALL_FORWARDING_MAX_CLASSX_OFFSET; j++) {
+            ACallForward record = supplementary->call_forward[i][j];
+            supplementary->call_forward[i][j] = NULL;
+            if (record) free(record);
+        }
+    }
+}
+
 ASupplementaryService
 asupplementary_create(int base_port, int instance_id)
 {
-    return &_s_supplementary[instance_id];
+    ASupplementaryService supplementary = &_s_supplementary[instance_id];
+
+    asupplementary_callforward_init(supplementary);
+
+    return supplementary;
 }
 
 void
 asupplementary_destroy(ASupplementaryService supplementary)
 {
-    /* nothing really */
+    asupplementary_callforward_destroy(supplementary);
+}
+
+int
+asupplementary_set_call_forward(ASupplementaryService supplementary,
+                                int reason, int classx_offset, bool enabled,
+                                char* number, int toa, int time)
+{
+    ACallForward* record = NULL;
+
+    if (reason < 0 || reason > CALL_FORWARDING_MAX_REASON ||
+        classx_offset < 0 || classx_offset > CALL_FORWARDING_MAX_CLASSX_OFFSET) {
+        return -1;
+    }
+
+    record = &supplementary->call_forward[reason][classx_offset];
+    if (!*record) {
+        *record = (ACallForward) malloc(sizeof(ACallForwardRec));
+        memset(*record, 0, sizeof(ACallForwardRec));
+    }
+
+    (*record)->enabled = enabled;
+    (*record)->toa     = toa;
+    (*record)->time    = time;
+    strncpy((*record)->number, number, CALL_FORWARDING_MAX_NUMBERS);
+
+    return 0;
+}
+
+int
+asupplementary_remove_call_forward(ASupplementaryService supplementary,
+                                   int reason, int classx_offset)
+{
+    ACallForward* record = NULL;
+
+    if (reason < 0 || reason > CALL_FORWARDING_MAX_REASON ||
+        classx_offset < 0 || classx_offset > CALL_FORWARDING_MAX_CLASSX_OFFSET) {
+        return -1;
+    }
+
+    record = &supplementary->call_forward[reason][classx_offset];
+
+    if (*record) {
+        free(*record);
+        *record = NULL;
+    }
+
+    return 0;
+}
+
+ACallForward
+asupplementary_get_call_foward(ASupplementaryService supplementary,
+                               int reason, int classx_offset)
+{
+    if (reason < 0 || reason > CALL_FORWARDING_MAX_REASON ||
+        classx_offset < 0 || classx_offset > CALL_FORWARDING_MAX_CLASSX_OFFSET) {
+        return NULL;
+    }
+
+    return supplementary->call_forward[reason][classx_offset];
 }
--- telephony/supplementary_service.h
+++ telephony/supplementary_service.h
@@ -12,9 +12,60 @@
 #ifndef _android_supplementary_service_h
 #define _android_supplementary_service_h
 
+/* call forwarding service classx, see +CCFC commands */
+typedef enum {
+    A_CALL_FORWARDING_CLASSX_OFFSET_VOICE = 0,
+    A_CALL_FORWARDING_CLASSX_OFFSET_DATA,
+    A_CALL_FORWARDING_CLASSX_OFFSET_FAX,
+    A_CALL_FORWARDING_CLASSX_OFFSET_SMS,
+    A_CALL_FORWARDING_CLASSX_OFFSET_DATA_SYNC,
+    A_CALL_FORWARDING_CLASSX_OFFSET_DATA_ASYNC,
+    A_CALL_FORWARDING_CLASSX_OFFSET_PACKET,
+    A_CALL_FORWARDING_CLASSX_OFFSET_PAD
+} ACallForwardingClassxOffset;
+
+/* call forwarding condition reason, see +CCFC commands */
+typedef enum {
+    A_CALL_FORWARDING_REASON_UNCONDITIONAL = 0,
+    A_CALL_FORWARDING_REASON_BUSY,
+    A_CALL_FORWARDING_REASON_NO_REPLY,
+    A_CALL_FORWARDING_REASON_NOT_REACHABLE,
+    A_CALL_FORWARDING_REASON_ALL,
+    A_CALL_FORWARDING_REASON_ALL_CONDITIONAL
+} ACallForwardingReason;
+
+/* call forwarding mode, see +CCFC commands */
+typedef enum {
+    A_CALL_FORWARDING_MODE_DISABLE = 0,
+    A_CALL_FORWARDING_MODE_ENABLE,
+    A_CALL_FORWARDING_MODE_QUERY,
+    A_CALL_FORWARDING_MODE_REGISTRATION,
+    A_CALL_FORWARDING_MODE_ERASURE
+} ACallForwardingMode;
+
+#define CALL_FORWARDING_MAX_NUMBERS       32
+#define CALL_FORWARDING_MAX_REASON        A_CALL_FORWARDING_REASON_NOT_REACHABLE
+#define CALL_FORWARDING_MAX_CLASSX_OFFSET A_CALL_FORWARDING_CLASSX_OFFSET_PAD
+
 typedef struct ASupplementaryServiceRec_*    ASupplementaryService;
+
+typedef struct ACallForwardRec_ {
+    bool  enabled;
+    char  number[CALL_FORWARDING_MAX_NUMBERS + 1];
+    int   toa;
+    int   time;
+} ACallForwardRec, *ACallForward;
 
 extern ASupplementaryService  asupplementary_create( int base_port, int instance_id );
 extern void                   asupplementary_destroy( ASupplementaryService sim );
+
+extern int  asupplementary_set_call_forward( ASupplementaryService supplementary,
+                                             int reason, int classx_offset,
+                                             bool enabled, char* number, int toa,
+                                             int time);
+extern int  asupplementary_remove_call_forward( ASupplementaryService supplementary,
+                                                int reason, int classx_offset );
+extern ACallForward  asupplementary_get_call_foward( ASupplementaryService supplementary,
+                                                     int reason, int classx_offset );
 
 #endif /* _android_supplementary_service_h */
